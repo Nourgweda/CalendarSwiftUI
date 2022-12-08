0ad217a8ee925d8fcd134d19864f336586088c76
Nourgweda <37792672+Nourgweda@users.noreply.github.com>
2022-12-08 12:29:40 +0200

    Initial Commit

---
+++ CalendarSwiftUI/CalendarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarSwiftUIApp.swift
+//  CalendarSwiftUI
+//
+//  Created by Nour Gweda on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalendarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
